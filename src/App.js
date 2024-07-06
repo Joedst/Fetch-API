14fc30ab2ff69c9b61b4c6811633351e5e470639
joedst <joeledstrom@outlook.com>
2024-07-06 17:34:39 +0200

    Added some HTML and Design

--- src/App.js
+++ src/App.js
@@ -6,34 +6,42 @@ import './App.css';
 import React, { useEffect, useState } from "react"
 
 const App = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchUserData = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        return response.json()
-      })
+      .then(response => response.json())
       .then(data => {
-        setUsers(data)
-      })
-  }
+        setUsers(data);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
+    fetchUserData();
+  }, []);
 
   return (
     <div>
-      {users.length > 0 && (
-        <ul>
+      <header>
+        <h1>FBI's Most Wanted List</h1>
+      </header>
+      {loading && <div className="loading-spinner"></div>}
+      {!loading && users.length > 0 && (
+        <div className="card-container">
           {users.map(user => (
-            <li key={user.id}>{user.name}</li>
+            <div className="card" key={user.id}>
+              <div className="card-content">
+                <h3 className="card-title">{user.name}</h3>
+                <p className="card-description">{user.email}</p>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
-}
-
+};
 
 export default App;
